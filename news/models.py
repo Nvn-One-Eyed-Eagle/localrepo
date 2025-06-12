4f6c6626d9c58fffe2a225217b96442dd44e4e73
DevNayven <naveen.phartyal2023@gmail.com>
2025-06-12 17:27:41 +0530

    Initial commit: added News model and image upload support

--- news/models.py
+++ news/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
 # Create your models here.
 class News(models.Model):
     title = models.CharField(max_length=200)
     banner = models.ImageField(upload_to='news/banner')
     content = models.TextField(max_length=200)
     created_time = models.DateTimeField(auto_now_add=True)
-    updated_time = model.DateTimeField(auto_now=True)
-    
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(str):
+        return self.title
