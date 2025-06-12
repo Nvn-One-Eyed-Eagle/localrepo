46b2407817ff1071f338953f1ae8c01fafd3e73d
DevNayven <naveen.phartyal2023@gmail.com>
2025-06-12 16:43:14 +0530

    appcreated_properly

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    banner = models.ImageField(upload_to='news/banner')
+    content = models.TextField(max_length=200)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = model.DateTimeField(auto_now=True)
+    
