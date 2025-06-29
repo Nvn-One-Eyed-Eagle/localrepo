be4a6a2956529e310a80dca1c7e64c0cb4bbe4e0
DevNayven <naveen.phartyal2023@gmail.com>
2025-06-29 18:33:06 +0530

    middleware created

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import News
+from .models import News,Page
 admin.site.register(News)
 # Register your models here.
+admin.site.register(Page)
---
+++ news/middleware.py
@@ -0,0 +1,13 @@
+from .models import Page 
+class ViewCounterMiddleware :
+    def __init__(self , get_response):
+        self.get_response = get_response
+    def __call__ (self,request):
+        print("view counter middleware : ")
+        page = Page.objects.get_or_create(url = request.path)
+        page[0].views+=1
+        page[0].save()
+        request.page_views = page[0].views
+        print(request.page_views)
+        response = self.get_response(request)
+        return response
